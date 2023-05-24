99b224ed0ffb77ce6d6bfb9abe244f4a63c6b5e3
ROBINSON MOLINA <Robinson.molina01@uptc.edu.co>
2023-05-24 17:21:37 -0500

    Agregando un chanchito feliz

--- archivo2.java
+++ archivo2.java
@@ -3,6 +3,8 @@ public class archivo2 {
     public static void main(String[] args) {
         
         System.out.println("chancito feliz dos");
+
+        System.out.println("otro chanchito feliz");
         
     }
 }
