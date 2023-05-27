c39c8b0f500f911c87eefff8f24b0682258c48e8
ROBINSON MOLINA <Robinson.molina01@uptc.edu.co>
2023-05-26 19:17:16 -0500

    actualizando archivo 2

--- archivo2.java
+++ archivo2.java
@@ -3,5 +3,6 @@ public class archivo2 {
     public static void main(String[] args) {
         
         System.out.println("Otro chanchito feliz");
+        System.out.println("Otro chacnchito feliz");
     }
 }
