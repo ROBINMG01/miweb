85f509842df88f06f99c1cdf3ba75ef927bafb1f
ROBINSON MOLINA <Robinson.molina01@uptc.edu.co>
2023-05-26 19:02:59 -0500

    mostrando estatus corto

--- archivo1.java
+++ archivo1.java
@@ -3,5 +3,6 @@ public class archivo1{
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo");
+        System.out.println("otro chanchito feliz");
     }
 }
---
+++ archivo2.java
@@ -0,0 +1,7 @@
+public class archivo2 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Otro chanchito feliz");
+    }
+}
